baf5d5b861e8d19fcffed8b36507e90bce38c565
DjGuruge <djguruge@hotmail.it>
2025-02-08 01:45:38 +0100

    new branch

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import src.Arrays;
+
+public class Main{
+    public static void main(String[] args){
+        int[] numeri = {1,2,-83,4,5,6,-7,8,9,10};
+        Arrays.ritornaMinimo(numeri);
+        Arrays.ritornaMassimo(numeri);
+    //    Arrays arrays = new Arrays();
+       // Arrays.ritornaMinimo(numeri);
+        System.out.println("hello world");
+    }
+}
+//Implementare un metodo che prenda come parametro un array di interi e ne ritorni il
+// massimo
